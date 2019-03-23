2aadfc782316824216de96335fedc763d668dcf8
trdwll <git@trdwll.com>
2019-03-23 19:28:10 -0400

    add vendor and setup some of the project

---
+++ JKWSite/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import View
+from django.db.models import Q
+
+
+
+def handler404(request, exception, template_name='error_pages/404.html'):
+	return render(request, 'error_pages/404.html', {}, status=404)
+
+def handler500(request, exception, template_name='error_pages/500.html'):
+	return render(request, 'error_pages/500.html', {}, status=500)
