0603e7157440c5df3378641e4a503b1e9532bea9
trdwll <git@trdwll.com>
2019-03-25 15:48:11 -0400

    start design

--- SetupProject.bat
+++ SetupProject.bat
@@ -1,14 +1,20 @@
 @echo off
 
 rem compile scss
-echo Compiling SCSS to CSS
-pysassc vendor/bootstrap/scss/bootstrap.scss static/css/bootstrap/bootstrap.css
+rem echo Compiling SCSS to CSS
+rem pysassc vendor/bootstrap/scss/bootstrap.scss static/css/bootstrap/bootstrap.css
 
 rem minify css
-echo Minifying CSS
-css-html-js-minify static/css/bootstrap/bootstrap.css
+rem echo Minifying CSS
+rem css-html-js-minify static/css/bootstrap/bootstrap.css
 
 rem Copy vendor files
 echo Copying vendor files to static/*
-cp vendor/bootstrap/dist/js/bootstrap.min.js static/js/bootstrap/bootstrap.min.js
 cp vendor/Font-Awesome-Pro/css/all.min.css static/css/fontawesome/all.min.css
+xcopy /E %CD%\vendor\Font-Awesome-Pro\webfonts %CD%\static\webfonts
+
+cp vendor/gameforest/dist/css/theme.min.css static/css/theme.min.css
+cp vendor/gameforest/dist/js/theme.bundle.min.js static/js/theme.bundle.min.js
+cp vendor/gameforest/dist/js/theme.min.js static/js/theme.min.js
+
+xcopy /E %CD%\vendor\gameforest\dist\plugins %CD%\static\plugins
---
+++ static/js/custom.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    var notice = "&copy; 2018-" + new Date().getFullYear() + " Chain Gang Entertainment Inc. All rights reserved.";
+    $('.footer-copyright').append(notice);
+});
