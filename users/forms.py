63625d779363ae64e68641bdc1c2d8a700ac22ba
trdwll <git@trdwll.com>
2019-03-27 20:16:22 -0400

    login/register work

---
+++ users/forms.py
@@ -0,0 +1,62 @@
+from django import forms
+from django.contrib.auth.models import User
+
+#from .models import UserProfile
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-control-secondary',
+            'placeholder': 'Username'
+        }), min_length=4, label='Username', required=True, help_text='The username of your account.')
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control form-control-secondary', 
+            'value': '', 
+            'placeholder': 'Password'
+        }), min_length=8, label='Password', required=True, help_text='The password that you use to sign in your account.')
+
+    class Meta:
+        fields = [
+            'username',
+            'password'
+        ]
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'class': 'form-control form-control-secondary',
+            'placeholder': 'Username'
+        }), min_length=4, label='Username', required=True, help_text='Username that will be displayed around the website.')
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control form-control-secondary',
+            'placeholder': 'Email'
+        }), label='Email', required=True, help_text='We\'ll never share your email with anyone else.')
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control', 
+            'value': '', 
+            'placeholder': 'Password'
+        }), min_length=8, label='Password', required=True, help_text='The password that you\'ll use to sign in your account.')
+    
+    password_confirm = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control', 
+            'value': '', 
+            'placeholder': 'Confirm Password'
+        }), min_length=8, label='Password', required=True, help_text='The password that you\'ll use to sign in your account.')
+
+    class Meta:
+        fields = [
+            'username',
+            'email',
+            'password',
+            'password_confirm'
+        ]
+
--- users/views.py
+++ users/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
+from .forms import LoginForm, RegisterForm
 
 class LoginView(View):
-    template_name = 'users/register.html'
+    template_name = 'users/login.html'
 
     def get(self, request):
         # if request.user.is_authenticated:
            # return redirect('home_page')
-           
-        return render(request, self.template_name)
+
+        return render(request, self.template_name, {'form': LoginForm()})
+
+    def post(self, request):
+        # if request.user.is_authenticated:
+           # return redirect('home_page')
+
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # TODO: allow email authentication
+            username_field = form.cleaned_data['username']
+            password_field = form.cleaned_data['password']
+
+            # TODO: google recaptcha validation
+            if True:
+                user_obj = authenticate(username=username_field, password=password_field)
+                if user_obj is not None and user_obj.is_active:
+                    login(request, user_obj)
+                    return redirect('home_page')
+                else:
+                    messages.error(request, 'This user does not exist, please check the username and password.')
+            else:            
+                messages.error(request, 'Recaptcha invalid. Please try again.')
+        else:
+            messages.error(request, 'Something went wrong. Please try again.')
+
+        return render(request, self.template_name, {'form': form})
 
 
 class RegisterView(View):
@@ -19,5 +47,4 @@ class RegisterView(View):
        # if request.user.is_authenticated:
            # return redirect('home_page')
 
-        return render(request, self.template_name)
-
+        return render(request, self.template_name, {'form': RegisterForm()})
